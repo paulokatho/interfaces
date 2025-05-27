6a524470e10608fe77050c3012c4dccb50537b05
lara-encoding <analara.cafe@gmail.com>
2025-05-28 00:59:50 +0100

    feat: funcoes referenciando metodos

--- ExemploFunction/src/exercicios/lambda/calculo/Foerach.java
+++ ExemploFunction/src/exercicios/lambda/calculo/Foerach.java
@@ -22,7 +22,20 @@ public class Foerach {
         System.out.println("\nMethod Reference #01...");
         aprovados.forEach(System.out::println);
 
+        // Usando forEach com lambda 02
+        System.out.println("\nLambda #02...");
+        aprovados.forEach(nome -> meuImprimir(nome)); // Essa é uma sentença de código
+        // nome -> meuImprimir(nome) - Essa não é uma sentença de código, é uma expressão lambda!
+        // É uma expressão lambda que é passada como parametro para a função forEach
+
+        // Usando Method Reference 02
+        System.out.println("\nMethod Reference #02...");
+        aprovados.forEach(Foerach::meuImprimir); // Passo minha propria classe Foreach e o metodo meuImprimir como referência
+        // Esse metodo, meuImprimir, já tem tudo o que preciso e posso usar ele tranquilamente
 
     }
 
+    static void meuImprimir(String nome) {
+        System.out.println("Method Reference 02 - " + nome);
+    }
 }
