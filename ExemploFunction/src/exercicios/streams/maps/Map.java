45eed70b3ae94e9b490fb03ca26f24c48f9a8b43
lara-encoding <analara.cafe@gmail.com>
2025-06-07 18:27:22 +0100

    feat: stream - Entendendo como criar maps e usar com stream.

--- ExemploFunction/src/exercicios/streams/maps/Map.java
+++ ExemploFunction/src/exercicios/streams/maps/Map.java
@@ -12,6 +12,7 @@ public class Map {
         Consumer<String> print = System.out::print; // Só para facilitar a impressão
 
         List<String> marcas = Arrays.asList("BMW ", "Audi ", "Honda ");
+
         marcas.stream().map(m -> m.toUpperCase()).forEach(print);
 
         // UnaryOperator<String> maiuscula = n -> n.toUpperCase();
